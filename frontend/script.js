7e6a04c1e1c5bd905d93214e887f7819afb6d1da
BlueTot <bluetotkc@gmail.com>
2025-08-27 17:35:00 +0100

    feat: added custom css styles

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,61 @@
+function irclogLanguage(hljs) {
+    return { contains: [
+        {
+            // Match system messages starting with ***
+            begin: /\*{3}/,
+            end: /$/,
+            className: "comment",
+            contains: [
+                {
+                    className: "pink",
+                    begin: /(Quits|Joins|Parts|Nick|Topic|Kick)/
+                },
+                {
+                    className: "comment2",
+                    begin: /([^ \n]+ is now known as [^ \n]+)/
+                },
+                {
+                    className: "green",
+                    begin: /\b[A-Za-z0-9_\-]+\b/,
+                },
+                {
+                    className: "number",
+                    begin: /\(/,
+                    end: /\)/,
+                    contains: [
+                        { className: "comment", begin: /[^)]+/ }
+                    ]
+                },
+                {
+                    className: "number",
+                    begin: /\(/,
+                    end: /\)/,
+                    contains: [
+                        { className: "comment", begin: /[^)]+/ }
+                    ]
+                }
+            ]
+        },
+        {
+            className: 'purple',
+            begin: /\d{2}:\d{2}:\d{2}|\d{4}-\d{2}-\d{2}/
+        },
+        {
+            className: 'yellow',
+            begin: /<[^ >]+>/
+        },
+        {
+            className: 'blue',
+            begin: /[A-za-z0-9_\-]+: /
+        },
+        {
+            className: 'link',
+            begin: /(http[s]?:\/\/[^ \n]+)/
+        }
+    ]}
+}
+
+
 document.addEventListener("DOMContentLoaded", async () => {
 
     const params = new URLSearchParams(document.location.search);
@@ -9,19 +67,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     } else {
         const response = await fetch(`http://localhost:4000/log?url=${url}`);
         const text = await response.text();
-        console.log(text);
         codeElement.textContent = text;
 
-        hljs.registerLanguage("irc-log", function(hljs) {
-            return {
-                contains: [
-                    { className: 'number', begin: /\d{2}:\d{2}:\d{2}|\d{4}-\d{2}-\d{2}/ }, // timestamps
-                    { className: 'keyword', begin: /(Joins|Quits|Parts|Ping|Quit)/ },
-                    { className: 'string', begin: /<[^ >]+>/ }, // usernames in <>
-                    { className: 'comment', begin: /(\*\*\*)/ }
-                ]
-            };
-        });
+        hljs.registerLanguage("irc-log", irclogLanguage);
         hljs.highlightElement(codeElement);
     }
 });
